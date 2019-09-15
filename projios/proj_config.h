61395126de4e77c7a86f348bc10c0d6fca2afb3d
Andrew Tetlaw <andrew@tetlaw.id.au>
2019-09-15 16:26:25 +0800

    Project Maintenance
    - make framework `proj` instead of `projios`
    - remove references to unneeded files
    - update proj_config.h from proj project

--- projios/proj_config.h
+++ projios/proj_config.h
@@ -1,14 +1,26 @@
 /* src/proj_config.h.  Generated from proj_config.h.in by configure.  */
-/* src/proj_config.h.in.  Generated from configure.in by autoheader.  */
+/* src/proj_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler supports basic C++11 syntax */
+#define HAVE_CXX11 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+/* Define to 1 if the compiler supports -Wzero-as-null-pointer-constant */
+#define HAVE_GCC_WARNING_ZERO_AS_NULL_POINTER_CONSTANT 1
+
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the <jni.h> header file. */
+/* #undef HAVE_JNI_H */
+
 /* Define to 1 if you have the `m' library (-lm). */
 #define HAVE_LIBM 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
 
 /* Define to 1 if you have localeconv */
 #define HAVE_LOCALECONV 1
@@ -16,11 +28,17 @@
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
+/* Define if your pthreads implementation have PTHREAD_MUTEX_RECURSIVE */
+#define HAVE_PTHREAD_MUTEX_RECURSIVE /**/
+
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
@@ -37,20 +55,29 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Enabled for Java/JNI Support */
+/* #undef JNI_ENABLED */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
 /* Name of package */
 #define PACKAGE "proj"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "warmerdam@pobox.com"
+#define PACKAGE_BUGREPORT "https://github.com/OSGeo/PROJ/issues"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "PROJ Projections"
+#define PACKAGE_NAME "PROJ"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "PROJ Projections 6.2.0"
+#define PACKAGE_STRING "PROJ 6.2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "proj"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://proj.org"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "6.2.0"
@@ -59,4 +86,4 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "6.0.0"
+#define VERSION "6.2.0"
