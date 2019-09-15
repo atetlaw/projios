59f315204652e671cd86fdc02eb32d3aa351aa1b
Andrew Tetlaw <andrew@tetlaw.id.au>
2019-09-15 09:43:39 +0800

    Initial Commit

---
+++ proj-ios/proj_ios.h
@@ -0,0 +1,19 @@
+//
+//  proj_ios.h
+//  proj-ios
+//
+//  Created by Andrew Tetlaw on 15/9/19.
+//  Copyright © 2019 dexagogo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for proj_ios.
+FOUNDATION_EXPORT double proj_iosVersionNumber;
+
+//! Project version string for proj_ios.
+FOUNDATION_EXPORT const unsigned char proj_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <proj_ios/PublicHeader.h>
+
+
